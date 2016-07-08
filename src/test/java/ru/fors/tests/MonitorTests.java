15411196f9c4c1626845c5e15d52ce9ffd1dddae
morozovivan95 <morozov.ivan95@yandex.ru>
2016-07-08 11:30:32 +0300

    improve monitorTest

--- src/test/java/ru/fors/tests/MonitorTests.java
+++ src/test/java/ru/fors/tests/MonitorTests.java
@@ -1,34 +1,43 @@
 package ru.fors.tests;
 
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriverException;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import ru.fors.pages.MainPage;
 import ru.fors.pages.LoginPage;
 
-
 public class MonitorTests extends TestBase {
-	
-	
-	
-	@Test
-	@Parameters({"username", "password"})
-	public void monitorTests(String username, String password){
-		LoginPage loginPage = new LoginPage(driver);
+
+    @Test
+    @Parameters({"username", "password"})
+    public void monitorTests(String username, String password) {
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.waitForLoginPageLoaded();
         MainPage mainPage = loginPage.userLogin(username, password);
         mainPage.waitUntilMainPageLoaded();
         mainPage.userClickInfopanelMenu();
         mainPage.userClickSearchScript();
-        while (mainPage.isIncidentExist()==true){
-                mainPage.getAndClickIncidentNumber();
-                mainPage.userChangeStatus();
-                mainPage.switchToParentFrame();
-                mainPage.userSaveIncident();
-                mainPage.userSetIncidentToInWork();
-                            }
-        mainPage.userLogout();
-	}
-	
-
+        try {
+            while (mainPage.isIncidentExist()) {
+                try {
+                    mainPage.getAndClickIncidentNumber();
+                    mainPage.userChangeStatus();
+                    mainPage.switchToParentFrame();
+                    mainPage.userSaveIncident();
+                    mainPage.userSetIncidentToInWork();
+                } catch (TimeoutException te) {
+                    mainPage.reloadPage();
+                    mainPage.waitUntilMainPageLoaded();
+                    mainPage.userClickInfopanelMenu();
+                    mainPage.userClickSearchScript();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mainPage.userLogout();
+        }
+    }
 }
