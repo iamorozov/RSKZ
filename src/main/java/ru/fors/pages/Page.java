7803d4c2722ba6de86efd224149676dff5d68211
morozovivan95 <morozov.ivan95@yandex.ru>
2016-07-08 12:05:09 +0300

    style improvements

--- src/main/java/ru/fors/pages/Page.java
+++ src/main/java/ru/fors/pages/Page.java
@@ -9,48 +9,48 @@ import ru.fors.utils.PropertyLoader;
 
 public class Page {
 
-	protected WebDriver driver;
-	protected WebDriverWait wait;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
 
-	public Page(WebDriver driver) {
-		this.driver = driver;
-		wait = new WebDriverWait(driver, Long.parseLong(PropertyLoader.loadProperty("imp.wait")));
-	}
-	
-	public void type(By element, String string){
-		driver.findElement(element).clear();
-		driver.findElement(element).sendKeys(string);
-	}
-	
-	public void click(By element){
-        waitUntilElementPresent(element);
-		driver.findElement(element).click();
-	}
-	
-	public String getElementText(By element){
-        waitUntilElementPresent(element);
-		return driver.findElement(element).getText();
-	}
-
-    public void waitUntilElementPresent(By element){
-        try{
-            wait.until(ExpectedConditions.visibilityOfElementLocated(element));
-            System.out.println(element+" is visible");
-        } catch (Exception e){
-            System.out.println(e);
-        }
+    public Page(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Long.parseLong(PropertyLoader.loadProperty("imp.wait")));
     }
 
-    public void waitUntilFrameToBeAvaibleAndSwitchToIt(int element){
-        try{
-            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(element));
-            System.out.println(element+" is visible and switch");
-        } catch (Exception e){
-            System.out.println(e);
-        }
+    public void type(By element, String string) {
+        driver.findElement(element).clear();
+        driver.findElement(element).sendKeys(string);
     }
 
-    private static ExpectedCondition<Boolean> waitElementValue(final By element, final String value){
+    public void click(By element) {
+        waitUntilElementPresent(element);
+        driver.findElement(element).click();
+    }
+
+    public String getElementText(By element) {
+        waitUntilElementPresent(element);
+        return driver.findElement(element).getText();
+    }
+
+    public void waitUntilElementPresent(By element) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+        System.out.println(element + " is visible");
+    }
+
+    public boolean findIfElementVisible(By element) {
+        return !driver.findElements(element).isEmpty() && driver.findElements(element).get(0).isDisplayed();
+    }
+
+    public void waitUntilFrameToBeAvaibleAndSwitchToIt(int element) {
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(element));
+        System.out.println(element + " is visible and switch");
+    }
+
+    public void reloadPage() {
+        driver.navigate().refresh();
+    }
+
+    private static ExpectedCondition<Boolean> waitElementValue(final By element, final String value) {
         return new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver webDriver) {
@@ -59,16 +59,14 @@ public class Page {
         };
     }
 
-    public void waitUntilElementSetValue(By element, String value){
-        try{
+    public void waitUntilElementSetValue(By element, String value) {
+        try {
             wait.until(waitElementValue(element, value));
-            System.out.println(element+" has value: " +value);
-        } catch (Exception e){
-            System.out.println(e);
+            System.out.println(element + " has value: " + value);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-
-	
 
 }
