0183a55aff2811b89d47ed78df27a93bb5d405a3
morozovivan95 <morozov.ivan95@yandex.ru>
2016-07-08 12:33:41 +0300

    remove static from getInstance method add new field driverPath

--- src/main/java/ru/fors/utils/WebDriverFactory.java
+++ src/main/java/ru/fors/utils/WebDriverFactory.java
@@ -6,26 +6,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class WebDriverFactory {
-	
-	public static final String CHROME = "chrome";
-	public static final String FIREFOX = "firefox";
-	public static final String INTERNET_EXPLORER = "ie";
-	
-	public static WebDriver getInstance(Browser browser) {
 
-		WebDriver webDriver = null;
-		String browserName = browser.getName();
-		
-		if (CHROME.equals(browserName)) {
-			webDriver = new ChromeDriver();
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String INTERNET_EXPLORER = "ie";
 
-		} else if (FIREFOX.equals(browserName)) {
-			webDriver = new FirefoxDriver();
+    private String driverPath;
 
-		} else if (INTERNET_EXPLORER.equals(browserName)) {
-			webDriver = new InternetExplorerDriver();
+    public WebDriverFactory(String driverPath) {
+        this.driverPath = driverPath;
+    }
 
-		} return webDriver;
-	}
+    public WebDriver getInstance(Browser browser) {
+
+        WebDriver webDriver = null;
+        String browserName = browser.getName();
+
+        if (CHROME.equals(browserName)) {
+            System.setProperty("webdriver.chrome.driver", driverPath);
+            webDriver = new ChromeDriver();
+
+        } else if (FIREFOX.equals(browserName)) {
+            webDriver = new FirefoxDriver();
+
+        } else if (INTERNET_EXPLORER.equals(browserName)) {
+            webDriver = new InternetExplorerDriver();
+
+        }
+        return webDriver;
+    }
 
 }
